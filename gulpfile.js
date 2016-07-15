14845cb51c38852aeed58316fb16df73a2f6a282
alexanderkozhevin <turboasm21@gmail.com>
2016-07-15 15:33:08 +0300

    deployment test #2

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require('gulp');
 var gulpSass = require('gulp-sass');
+var gulpCopy = require('gulp-copy');
 
 gulp.task('style',  function() {
-	return gulp.src('style/**/*.scss')
+	return gulp.src('src/style/**/*.scss')
 	.pipe(gulpSass({ errLogToConsole: true, sourceComments: 'map', sourceMap: 'sass'}))
-	.pipe(gulp.dest('./'))
+	.pipe(gulp.dest('production'))
 });
 
+gulp.task('copyIndex', function() {
+  gulp.src("src/index.html").pipe(gulpCopy('production/', {prefix: 1}));
+});
 
-gulp.task('default', ['style'], function(){
+gulp.task('default', ['style', 'copyIndex'], function(){
   process.exit(0);
 })
